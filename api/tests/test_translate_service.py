1781ddcfff827e54c74b3df9fee30a2e92882cb2
Mohit N. Vashisth <mohitnarayanvashisth2703@gmail.com>
2025-03-31 10:14:46 +0530

    minor changes in test_translate

--- api/tests/test_translate_service.py
+++ api/tests/test_translate_service.py
@@ -1,23 +1,15 @@
+from httpx import AsyncClient
+from hypothesis import given, strategies as st
 from api.main import app
-from api.schemas.language_translate import LanguageRequest
 from api.core.config import TRANSLATE_EP
 
 import pytest
-from fastapi import FastAPI
-from fastapi.testclient import TestClient
 
-client = TestClient(app)
+@pytest.mark.asyncio
+@given()
 
-@pytest.mark.parametrize("text, dest, status_code", [
-    ("", "en", 422),
-    ("hello", "it", 422),
-    ("Hello", "", 422),
-    ("Hello", "xyz", 422),
-    ("A" * 10000, "hi", 422),
-    (123, "hi", 422),
-])
-
-def test_translate(text, dest, status_code):
-    request_data = {"text": text, "dest": dest}
-    response = client.post(TRANSLATE_EP, json=request_data)
-    assert response.status_code == status_code, f"Unexpected status: {response.status_code}, body: {response.text}"
+async def test_translate(text, dest, status_code):
+    async with AsyncClient(base_url="http://localhost:8000") as ac:
+        request_data = {"text": text, "dest": dest}
+        response = await ac.post(TRANSLATE_EP, json=request_data)
+        assert response.status_code == status_code, f"Unexpected status: {response.status_code}, body: {response.text}"

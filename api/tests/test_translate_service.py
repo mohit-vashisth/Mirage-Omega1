09b5322f8e1829643416c7d6c5b4ece2d7d17520
Mohit N. Vashisth <mohitnarayanvashisth2703@gmail.com>
2025-03-31 10:14:57 +0530

    minor changes

--- api/tests/test_translate_service.py
+++ api/tests/test_translate_service.py
@@ -6,8 +6,14 @@ from api.core.config import TRANSLATE_EP
 import pytest
 
 @pytest.mark.asyncio
-@given()
-
+@pytest.mark.parametrize("text, dest, status_code", [
+    ("", "en", 422),
+    ("hello", "it", 422),
+    ("Hello", "", 422),
+    ("Hello", "xyz", 422),
+    ("A" * 10000, "hi", 422),
+    (123, "hi", 422),
+])
 async def test_translate(text, dest, status_code):
     async with AsyncClient(base_url="http://localhost:8000") as ac:
         request_data = {"text": text, "dest": dest}
